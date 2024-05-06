9e3409775b38dcb0ae407e943e2a65075f790ff5
Erick Sebastian Caiza Quishpe <151951589+ericksebastian2003@users.noreply.github.com>
2024-05-06 07:32:37 -0500

    Fundamentals

---
+++ clase03.js
@@ -0,0 +1,23 @@
+//Variables simples
+let numberOfPeople = 43
+const itemPrice = 45.67
+
+let shoppingList =["Apples","Milk"]
+const personInf={
+    name:"Erick",
+    city:"Quito"
+}
+
+
+//console.log(numberOfPeople);
+//Se ejecuta medciante node por   node nmbreArchivo.JS
+//Para ejecutar en tiempo real  medciante node : node --watch nombreArchivo.JS
+//typeof sirve para ver que tipo de dato es la variable
+//console.log( typeof numberOfPeople);
+//Strings-cadena de caracteres
+let gameType ="Racing and sports"
+//console.log( typeof gameType);
+//Propiedades
+gameType.length //Longitud
+//Métodos -- usa () al final
+gameType.toLocaleUpperCase() //Convertir toda la cadena a mayusculas
